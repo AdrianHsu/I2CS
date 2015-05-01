2af38116b08e8daf4dbd3c6af9c620793487bda3
AdrianHsu <adrianhsu1995@gmail.com>
2015-05-01 15:52:44 +0800

    RESTART

--- HW#4/hw4.cpp
+++ HW#4/hw4.cpp
@@ -24,7 +24,7 @@ double *real_h;
 double shortest_path(string &);
 void readParameters()
 {
-    ifstream ifs("input1", ifstream::binary);
+    ifstream ifs("input3", ifstream::binary);
 
     ifs.read((char*)&m, sizeof(int));
     ifs.read((char*)&n, sizeof(int));
@@ -59,14 +59,13 @@ void release()
 int main()
 {
     readParameters();
-
     /*for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < m + 1; j++)
             cout << v[i][j] << " ";
         cout << endl;
     }
-    cout << endl;
+    cout << "AA"<< endl;
     for(int i = 0; i < n + 1; i++)
     {
         for(int j = 0; j < m; j++)
@@ -82,65 +81,4 @@ int main()
 }
 double shortest_path(string& str)
 {
-    int x = 0, y = 0;
-    double total = 0;
-    srand(time(NULL));
-    
-    //the total num of move is exactly (m - 1) + (n - 1) for sure
-    while(!(x == m && y == n))
-    {
-        char dir = '\0'; //direction
-        bool special = 0;
-        //special case
-        if(y == n)
-        {
-            total += h[y][x];
-            x += 1;
-            dir = 'h';
-            special = 1;
-        }
-        else if(x == m)
-        {
-            total += v[y][x];
-            y += 1;
-            dir = 'v';
-            special = 1;
-        }
-        if(special)
-        {
-            str += dir;
-            continue;
-        }
-        //general case
-        if(v[y][x] > h[y][x] && y < (n + 1) && x < m)
-        {
-            total += h[y][x];
-            x += 1;
-            dir = 'h';
-        }
-        else if(v[y][x] < h[y][x] && y < n && x < (m + 1))
-        {
-            total += v[y][x];
-            y += 1;
-            dir = 'v';
-        }
-        else //v[y][x] == h[y][x], random
-        {
-            int _random = rand() % 2;
-            if(_random == 1 && y < (n + 1) && x < m) //move horizontally
-            {
-                total += h[y][x];
-                x += 1;
-                dir = 'h';
-            }
-            else if( _random == 0 && y < n && x < (m + 1)) // move vertically
-            {
-                total += v[y][x];
-                y += 1;
-                dir = 'v';
-            }
-        }
-        str += dir;
-    }
-    return total;
 }
