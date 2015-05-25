fa3d0d2e5880f52d0936cbba1d5135dd84f925f2
AdrianHsu <adrianhsu1995@gmail.com>
2015-05-25 21:05:16 +0800

    done

--- HW#5/t_main.cpp
+++ HW#5/t_main.cpp
@@ -8,9 +8,27 @@ using namespace std;
 int main()
 {
     TVector<3> a,b,c; b.set(0,0).set(1,1).set(2,2); 
+    
     c = b;
     a = b+c;
     cout << a << endl;
+    a.set(0, 0.0).set(1, 0.1).set(2, 0.2);
 
+    cout << a << endl;
+    cout << a.get(1) << endl;
+    cout << a.getLength() << endl;
+
+    c = b = a;            // test copier
+
+    cout << b << endl;
+    cout << c << endl;
+
+    TVector<3> d(c);  // test copy constructor
+    cout << d << endl;
+
+    cout << c*d << endl;     // inner product
+    cout << a+b+c << endl;
+
+    cout << a+b+c << endl;
 }
 
