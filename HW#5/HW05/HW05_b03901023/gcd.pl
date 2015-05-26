5ecfdf3d3837247906ad01de8002f3ec0fb55350
AdrianHsu <adrianhsu1995@gmail.com>
2015-05-26 16:46:37 +0800

    gcd

--- HW#5/HW05/HW05_b03901023/gcd.pl
+++ HW#5/HW05/HW05_b03901023/gcd.pl
@@ -3,5 +3,5 @@ A is 0 -> G is B;
 B is 0 -> G is A;
 A > 0,
 B > 0,
-A <  B -> A1 is B - A, B1 is A, gcd(A1,B1,G1), G is G1.
-A >= B -> A1 is A - B, B1 is B, gcd(A1,B1,G1), G is G1;
+A <  B -> A1 is B - A, B1 is A, gcd(A1,B1,G1), G is G1;
+A >= B -> A1 is A - B, B1 is B, gcd(A1,B1,G1), G is G1.
