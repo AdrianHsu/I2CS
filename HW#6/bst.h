95aafa58a2ecb77c02d1d360f2ad98ca563b59fd
AdrianHsu <adrianhsu1995@gmail.com>
2015-05-29 20:41:12 +0800

    bst first commit

--- HW#6/bst.h
+++ HW#6/bst.h
@@ -18,6 +18,107 @@ class Node {
 
 class BST {
 
+    public:
+        BST() { root = NULL; }
+
+        ~BST() 
+        {
+            if (root != NULL)
+                release(root);
+        }
+        void postorder() const {
+            if (root == NULL) 
+                return;
+            postorder(root);
+            cout << endl;
+        }
+        void inorder() const {
+            if (root == NULL) 
+                return;
+            inorder(root);
+            cout << endl;
+        }
+    
+        void insert(int val) {
+            if (root == NULL) 
+                root = new Node(val);
+            else
+                insert(root, val);
+        }
+
+        /*BONUS: DELETE FUNCION*/
+        void _delete(int val) {
+            
+            Node* target = root;
+            Node* parent;
+            bool left_child;
+            //while loop binary search
+            while(true)
+            {
+                if(val == target->data)
+                    break;
+                else if(val < target->data)
+                {
+                    if(target->left == NULL) return; //ERROR: target not found
+                    parent = target;
+                    target = target->left;
+                    left_child = 1;
+                }
+                else
+                {
+                    if(target->right == NULL) return;
+                    parent = target;
+                    target = target->right;
+                    left_child = 0;
+                }
+            }
+            if(target->left == NULL && target->right == NULL) //delete leaf node
+            {
+                if(left_child) parent->left = NULL;
+                else parent->right = NULL;
+                delete target;
+                return;
+            }
+            if(target->right == NULL)
+            {
+                parent->left = target->left;
+                delete target;
+                return;
+            }
+            else if(target->left == NULL)
+            {
+                parent->right = target->right;
+                delete target;
+                return;
+            }
+            else
+            {
+                parent = target;
+                Node *tmp = target;
+                target = target->right;
+                while(target->left != NULL)
+                {
+                    parent = target;
+                    target = target->left;
+                }
+                //swap
+                Node *swap = target;
+                target = tmp;
+                tmp = swap;
+                if(target->right == NULL)
+                {
+                    parent->left = NULL;
+                    delete target;
+                    return;
+                }
+                else
+                {
+                    parent->left = target->right;
+                    delete target;
+                    return;
+                }
+            }
+        }
     protected:
         
         Node *root;
@@ -65,38 +166,7 @@ class BST {
                     insert(p->right, val);
             }
         }
-    public:
-        BST() { root = NULL; }
 
-        ~BST() 
-        {
-            if (root != NULL)
-                release(root);
-        }
-        void postorder() const {
-            if (root == NULL) 
-                return;
-            postorder(root);
-            cout << endl;
-        }
-        void inorder() const {
-            if (root == NULL) 
-                return;
-            inorder(root);
-            cout << endl;
-        }
-    
-        void insert(int val) {
-            if (root == NULL) 
-                root = new Node(val);
-            else
-                insert(root, val);
-        }
-
-        /*BONUS: DELETE FUNCION*/
-        void _delete(int val) {
-            //TODO
-        }
 };
 
 #endif
--- HW#6/testBst.cpp
+++ HW#6/testBst.cpp
@@ -4,5 +4,12 @@ using namespace std;
 #include "bst.h"
 int main()
 {
+	BST bst;
+
+	for(int i = 1; i < 8; i++)
+		bst.insert(i);
+	bst.inorder();
+	bst._delete(3);
+	bst.inorder();
 	return 0;
 }
