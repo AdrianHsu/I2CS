170eee31ed0fc8ff0154b4ebfcd317a6012bae2e
AdrianHsu <adrianhsu1995@gmail.com>
2015-05-30 02:10:21 +0800

    done

--- HW#6/bst.h
+++ HW#6/bst.h
@@ -53,60 +53,77 @@ class BST {
             {
                 if(root->right == NULL && root->left == NULL){
                     delete root;
+                    root = NULL;
                     return;
                 }
                 if(root->right == NULL){
-                    root = root->left;
+                    Node* tmp = root;
+                    tmp = tmp->left;
+                    delete root;
+                    root = tmp;
                     return;
                 }
                 else if(root->left == NULL){ //for 1-degree nodes
-                    root = root->right;
+                    Node* tmp = root;
+                    tmp = tmp->right;
+                    delete root;
+                    root = tmp;
                     return;
                 }
             }
             Node* target = root;
             Node* parent = root;
             bool left_child; // no need to init; if the target is parent's left child(true)
-            //while loop binary search
+            
+            //while loop binary search, find target's address
             while(true)
             {
                 if(val == target->data)
                     break;
-
                 parent = target;
                 if(val < target->data)
                 {
-                    if(target->left == NULL) return; //ERROR: target not found                    
+                    if(target->left == NULL)
+                    {
+                        cerr << "ERROR: target not found." << endl;
+                        return;                  
+                    }
                     target = target->left;
                     left_child = true;
                 }
                 else
                 {
-                    if(target->right == NULL) return; //ERROR: target not found
+                    if(target->right == NULL)
+                    {
+                        cerr << "ERROR: target not found." << endl;
+                        return;                  
+                    }
                     target = target->right;
                     left_child = false;
                 }
             }
-            if(target->left == NULL && target->right == NULL) //delete leaf node
+            if(target->left == NULL && target->right == NULL) //case: leaf node
             {
                 if(left_child) parent->left = NULL;
                 else parent->right = NULL;
+                
+                delete target;
+                return;
             }
-
-            if(target->right == NULL)
+            if(target->right == NULL) //case: 1-degree node
             {
                 if(left_child)parent->left = target->left;
                 else parent->right = target->left;
             }
-            else if(target->left == NULL) //for 1-degree nodes
+            else if(target->left == NULL) //case: 1-degree node
             {
                 if(left_child)parent->left = target->right;
                 else parent->right = target->right;
             }
-            else// The minimum node of the right subtree (keep going left).
-            {   // (replacing with its successor for 2-degree nodes)
+            else    
+            {   // replacing with its "successor" for 2-degree nodes
+                // e.g.the minimum node of the right subtree (keep going left).
                 Node* _root = target;
-                
                 parent = target;
                 target = target->right;
                 if(target->left == NULL)
